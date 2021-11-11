d7b1c14b0709245bfc2f2294de7b686322acd070
mina-mina-ueda <91769651+mina-mina-ueda@users.noreply.github.com>
2021-11-11 11:29:47 +0000

    [Add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,7 +13,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to books_path
+    redirect_to book_path
   end
  
   def edit
@@ -35,6 +35,6 @@ class BooksController < ApplicationController
   
   private
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title, :body)#strong parameters
   end
 end
