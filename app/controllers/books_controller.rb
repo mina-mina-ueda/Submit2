e68ba9b6c37b589f343976bdbab59b05addf1936
mina-mina-ueda <91769651+mina-mina-ueda@users.noreply.github.com>
2021-11-12 07:21:48 +0000

    [Add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,8 +13,11 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    if book.save
+      redirect_to book_path(book.id), notice: 'Book was Successfully created'
+    else
+      render :new
+    end
   end
  
   def edit
@@ -23,13 +26,17 @@ class BooksController < ApplicationController
   
   def update
     book = Book.find(params[:id])#←show
-    book.update(book_params)
-    redirect_to book_path(book)
+    if book.update(book_params)
+      redirect_to book_path(book), notice; 'Book was Successfully updated'
+    else
+      render :new
+    end
   end
   
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    redirect_to book_path
   end
   
   
